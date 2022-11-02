93852345e2cf42371e8b1571486d55b603762d69
ashbyp <ashbyp@yahoo.co.uk>
2022-11-02 20:31:53 +0000

    knn

---
+++ knn/knn.py
@@ -0,0 +1,17 @@
+
+
+class KNN:
+
+    def __init__(self, k=3):
+        self._k = k
+
+    def fit(self, X, y ):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+
+if __name__ == '__main__':
+    print('hello world...')
